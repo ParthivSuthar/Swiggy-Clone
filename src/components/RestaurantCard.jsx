95e5ae8c9c74dfc5a30f94e18361b54342dc89b5
Parthiv Suthar <myworkflow7070@gmail.com>
2025-06-07 22:24:15 +0530

    Create separate component for Restaurant Card

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function RestaurantCard( info ) {
+  return (
+    <>
+      <div className="min-w-[295px] h-[182px] relative" key={info?.id}>
+        <img
+          className="w-full h-full  rounded-2xl object-cover"
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_376,h_376/${info?.info?.cloudinaryImageId}`}
+        />
+        <div className="bg-gradient-to-t from-black from-1% to-transparent to-40%  rounded-2xl w-full h-full  absolute top-0"></div>
+        <p className="absolute bottom-0 text-white text-2xl ml-2 mb-1 font-bold">
+          {info?.info?.aggregatedDiscountInfoV3
+            ? info?.info?.aggregatedDiscountInfoV3?.header +
+              " " +
+              info?.info?.aggregatedDiscountInfoV3?.subHeader
+            : ""}
+          {console.log(info)}
+        </p>
+      </div>
+      <div className="mt-3">
+        <h2 className="text-lg font-semibold ">{info?.info?.name}</h2>
+        <p className="flex items-center gap-1 text-base font-semibold">
+          <i className="fi fi-ss-circle-star text-green-600 text-lg mt-1"></i>
+          {info?.info?.avgRating} . <span>{info?.info?.sla?.slaString}</span>
+        </p>
+        <p className="line-clamp-1 text-black/60 font-medium">
+          {info?.info?.cuisines?.join(", ")}
+        </p>
+        <p className="line-clamp-1 text-black/60 font-medium">
+          {info?.info?.locality}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default RestaurantCard;
--- src/components/TopRestaurant.jsx
+++ src/components/TopRestaurant.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
 
 function TopRestaurant({ data }) {
   const [value, setValue] = useState(0);
@@ -67,29 +68,7 @@ function TopRestaurant({ data }) {
         } */}
         {data.map((info) => (
           <div className="hover:scale-95 duration-200" key={info?.id}>
-            <div className="min-w-[295px] h-[182px] relative" key={info?.id}>
-              <img
-                className="w-full h-full  rounded-2xl object-cover"
-                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_376,h_376/${info?.info?.cloudinaryImageId}`}
-              />
-              <div className="bg-gradient-to-t from-black from-1% to-transparent to-40%  rounded-2xl w-full h-full  absolute top-0"></div>
-              <p className="absolute bottom-0 text-white text-2xl ml-2 mb-1 font-bold">
-                {info?.info?.aggregatedDiscountInfoV3
-                  ? info?.info?.aggregatedDiscountInfoV3?.header +
-                    " " +
-                    info?.info?.aggregatedDiscountInfoV3?.subHeader
-                  : ""}
-                {console.log(info)}
-              </p>
-            </div>
-            <div className="mt-3">
-              <h2 className="text-lg font-semibold ">{info?.info?.name}</h2>
-              <p className="flex items-center gap-1 text-base font-semibold"><i className="fi fi-ss-circle-star text-green-600 text-lg mt-1"></i>{info?.info?.avgRating} . <span>{info?.info?.sla?.slaString}</span></p>
-              <p className="line-clamp-1 text-black/60 font-medium">{info?.info?.cuisines?.join(", ")}</p>
-              <p className="line-clamp-1 text-black/60 font-medium">
-                    {info?.info?.locality}
-                </p>
-            </div>
+            <RestaurantCard {...info} />
           </div>
         ))}
       </div>
