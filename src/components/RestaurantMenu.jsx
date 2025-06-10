09eaa9166abffa5f9d0a34ec1f27453ce2468615
Parthiv Suthar <myworkflow7070@gmail.com>
2025-06-10 23:30:35 +0530

    Get All Required Data from Api and Create Restaurant Menu Card

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,25 +1,89 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router-dom";
 
 function RestaurantMenu() {
   const { id } = useParams();
   // let mainId = id.split("-").at(-1).split("rest").replace(",", "")
   let mainId = id.split("-").at(-1).replace(",", "");
   // console.log(mainId);
-  const [menuData, setMenuData] = useState("")
+  const [menuData, setMenuData] = useState([]);
+  const [resData, setResData] = useState([]);
+  const [discountData, setDiscountData] = useState([]);
 
-  async function fetchMenu(){
-      const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6126255&lng=77.04108959999999&restaurantId=${mainId}&catalog_qa=undefined&submitAction=ENTER`)
-      const res = await data.json()
-      // console.log(res?.data?.cards[0]?.card?.card?.text);
-      setMenuData(res?.data?.cards[0]?.card?.card?.text)
+  async function fetchMenu() {
+    const data = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6126255&lng=77.04108959999999&restaurantId=${mainId}&catalog_qa=undefined&submitAction=ENTER`
+    );
+    const res = await data.json();
+    // console.log(res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card);
+    // console.log(res?.data?.cards[2]?.card?.card?.info);
+    // console.log(res?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers);
+    setResData(res?.data?.cards[2]?.card?.card?.info);
+    setDiscountData(
+      res?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers
+    );
+    setMenuData(
+      res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+        ?.card
+    );
   }
 
   useEffect(() => {
     fetchMenu();
-  }, [])
-  return <div>RestaurantMenu - {mainId} {menuData}</div>
-  
+  }, []);
+  return (
+    <div className="w-full">
+      <div className="w-[800px] mx-auto pt-8">
+        <p className="text-[12px] text-slate-500 ">
+          {" "}
+          <Link to={"/"}>
+            {" "}
+            <span className="hover:text-slate-700 hover:cursor-pointer">
+              Home
+            </span>
+          </Link>{" "}
+          /{" "}
+          <Link to={"/"}>
+            {" "}
+            <span className="hover:text-slate-700 hover:cursor-pointer">
+              {resData.city}
+            </span>
+          </Link>{" "}
+          / <span className="text-slate-700">{resData.name}</span>
+        </p>
+        <h1 className="font-bold pt-6 text-2xl">{resData.name}</h1>
+        <div className="w-full h-[206px] bg-gradient-to-t px-4 pb-4 from-slate-200/70  mt-3 rounded-[30px]">
+          <div className="w-full border  border-slate-200/70 rounded-[30px] h-full bg-white">
+            <div className="flex items-center gap-1 font-semibold">
+              <i className="fi fi-ss-circle-star mt-1 text-green-600 text-lg"></i>
+              <span>{resData.avgRating}</span>
+              <span>({resData.totalRatingsString})</span>.
+              <span>{resData.costForTwoMessage}</span>
+            </div>
+            <p className="underline font-semibold text-orange-600 ">
+              {resData?.cuisines?.join(", ")}
+            </p>
+            <div className="flex gap-2 mt-2"  >
+              <div className="w-[9px] flex flex-col justify-center items-center">
+                <div className="w-[7px] h-[7px] bg-gray-500 rounded-full"></div>
+                <div className="w-[1px] h-[25px] bg-gray-500 "></div>
+                <div className="w-[7px] h-[7px] bg-gray-500 rounded-full"></div>
+              </div>
+              <div className="flex flex-col gap-1 text-sm font-semibold">
+                <p>
+                  Outlet{" "}
+                  <span className="text-gray-500 font-normal">
+                    {resData.locality}
+                  </span>
+                </p>
+                <p>{resData.sla?.slaString}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RestaurantMenu;
