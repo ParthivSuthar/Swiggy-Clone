26b5a59d7226121a3aee1bf024b533ce0cda9b22
Parthiv Suthar <myworkflow7070@gmail.com>
2025-07-19 11:12:54 +0530

    Add Clear Cart and Total Price functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,12 +5,28 @@ import { Link } from "react-router-dom";
 function Cart() {
   const { cartData, setCartData } = useContext(CartContext);
 
-  function handleRemoveFromCart(i){
-    let newArr = [...cartData]
-    newArr.splice(i,1)
-    setCartData(newArr)
-    localStorage.setItem("cartData", JSON.stringify(newArr))
+  function handleRemoveFromCart(i) {
+    let newArr = [...cartData];
+    newArr.splice(i, 1);
+    setCartData(newArr);
+    localStorage.setItem("cartData", JSON.stringify(newArr));
   }
+
+  function handleClearCart(){
+    setCartData([])
+    // localStorage.setItem("cartData",JSON.stringify([]))
+    localStorage.clear()
+  }
+  //   let totalPrice = 0;
+
+  //   for(let i=0; i<cartData.length; i++){
+  //     totalPrice = totalPrice + cartData[i].price / 100 || cartData[i].defaultPrice / 100
+  //   }
+
+  let totalPrice = cartData.reduce(
+    (acc, curVal) => acc + curVal.price / 100 || curVal.defaultPrice / 100,
+    0
+  );
 
   if (cartData.length === 0) {
     return (
@@ -30,7 +46,10 @@ function Cart() {
       <div className="w-[50%] mx-auto">
         {cartData.map((data, i) => (
           <div className="flex w-full justify-between my-5 p-2" key={i}>
-            <h2 className="w-[70%] text-3xl">{data.name}</h2>
+            <div className="w-[70%]">
+              <h2 className=" text-3xl">{data.name}</h2>
+              <p>Price - ₹{data.price / 100 || data.defaultPrice / 100}</p>
+            </div>
             <div className="w-[40%] md:w-[20%] relative h-full">
               <img
                 className="rounded-xl aspect-square"
@@ -40,12 +59,22 @@ function Cart() {
                 }
                 alt="recipeimg"
               />
-              <button onClick={() => handleRemoveFromCart(i)} className="bg-white absolute bottom-[-20px] left-1/2 -translate-x-1/2 text-base text-red-500 font-bold rounded-xl border px-5 py-2 drop-shadow">
+              <button
+                onClick={() => handleRemoveFromCart(i)}
+                className="bg-white absolute bottom-[-20px] left-1/2 -translate-x-1/2 text-base text-red-500 font-bold rounded-xl border px-5 py-2 drop-shadow"
+              >
                 Remove
               </button>
             </div>
           </div>
         ))}
+        <h2>Total - ₹{totalPrice}</h2>
+        <button
+          onClick={handleClearCart}
+          className="p-3 bg-green-600 rounded-lg my-7"
+        >
+          clear cart
+        </button>
       </div>
     </div>
   );
